2087021a1ed2e4101fc64850054a89a6d5a946a3
thxrhmn <rahmanmuhaemin@gmail.com>
2023-05-17 18:55:13 +0700

    change profile avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,7 @@ function Header() {
   return (
     <div className="flex items-center gap-5 mb-3">
       <div>
-        <img
-          src="https://i.pinimg.com/originals/47/4f/5f/474f5fa00f60fb5c2e47c9dfcd7b1593.jpg"
-          alt=""
-          width="100px"
-          height="100px"
-          className="rounded-full"
-        />
+        <div className="nf nf-fa-user_secret text-8xl"></div>
       </div>
       <div className="">
         <h1 className="text-3xl font-semibold">Rahman Muhaemin</h1>
