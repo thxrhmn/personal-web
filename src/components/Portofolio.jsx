74906006d5f8cbf39c8343dfaadab50ab24b16bf
thxrhmn <rahmanmuhaemin@gmail.com>
2023-05-17 19:55:15 +0700

    add gryscale portofolio image

--- src/components/Portofolio.jsx
+++ src/components/Portofolio.jsx
@@ -7,14 +7,14 @@ function Portofolio() {
       <h1 className="text-xl font-semibold mb-3">Portofolio</h1>
       <div className="flex flex-col md:flex-row gap-2 mb-3">
         <div className="border border-white border-solid rounded-md md:w-[50%] overflow-hidden">
-          <img src={Dumbflix} alt="" />
+          <img src={Dumbflix} alt="" className="grayscale hover:grayscale-0"/>
           <div className="px-2 pb-2">
             <h1>Dumbflix</h1>
             <div className="nf nf-dev-github_badge text-xl"></div>
           </div>
         </div>
         <div className="border border-white border-solid rounded-md md:w-[50%] overflow-hidden">
-          <img src={Holyways} alt="" />
+          <img src={Holyways} alt="" className="grayscale hover:grayscale-0"/>
           <div className="px-2 pb-2">
             <h1>Holyways</h1>
             <div className="nf nf-dev-github_badge text-xl"></div>
