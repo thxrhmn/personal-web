8e0326f494c53b98db57413e41558f75f45833fe
thxrhmn <rahmanmuhaemin@gmail.com>
2023-05-17 19:03:13 +0700

    fix notfound 404 vercel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
 function Header() {
   return (
     <div className="flex items-center gap-5 mb-3">
-      <div>
-        <div className="nf nf-fa-user_secret text-8xl"></div>
-      </div>
+      <div className="nf nf-fa-user_secret text-8xl"></div>
       <div className="">
         <h1 className="text-3xl font-semibold">Rahman Muhaemin</h1>
         <h3>Fullstack Developer</h3>
